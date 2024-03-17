62147bc1852cf5eb19c9b92e44f94b755ef44b21
Franco Canzani <francocanzani@gmail.com>
2024-03-17 21:20:37 +0100

    changes landing design

--- app/page.tsx
+++ app/page.tsx
@@ -15,43 +15,17 @@ export default function Page() {
         <h2 className='text-center text-pretty'>
           Welcome to <strong>Flamingo</strong>, the simple note taking app
         </h2>
-        <div className='flex flex-col sm:flex-row items-center justify-center space-x-3 space-y-5 sm:space-y-0'>
+        <div className='flex items-center justify-center space-x-3'>
           <Link
             href={'/notes'}
-            className='p-4 border-2 border-pink-400 rounded-md shadow shadow-pink-300 max-w-sm mx-auto space-y-2'
+            className='p-2 border-2 font-medium hover:bg-amber-300 text-sm border-amber-200 bg-amber-200 rounded-md shadow shadow-amber-100'
           >
-            <h2 className='flex items-center justify-center text-sm font-medium'>
-              Continue Anonymously
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='1em'
-                height='1em'
-                viewBox='0 0 20 20'
-                className='ml-1'
-              >
-                <path
-                  fill='currentColor'
-                  d='M15 2H5L4 8h12zM0 10s2 1 10 1s10-1 10-1l-4-2H4zm8 4h4v1H8z'
-                />
-                <circle cx='6' cy='15' r='3' fill='currentColor' />
-                <circle cx='14' cy='15' r='3' fill='currentColor' />
-              </svg>
-            </h2>
-            <ul className='text-sm'>
-              <li>✓ Quick access</li>
-              <li>✓ Preserves user privacy</li>
-              <li>✕ Risk of data loss</li>
-              <li>✕ No syncing across devices</li>
-            </ul>
+            Continue Local
           </Link>
-          <div className='p-4 border-2 border-pink-400 rounded-md shadow shadow-pink-300 max-w-sm mx-auto space-y-2'>
-            <LoginButton callbackUrl='/editor' />
-            <ul className='text-sm'>
-              <li>✓ Ability to save data </li>
-              <li>✓ Syncing across devices</li>
-              <li>✓ Enhanced security</li>
-            </ul>
-          </div>
+          <LoginButton
+            callbackUrl='/editor'
+            className='p-2 border-2 hover:bg-amber-300 text-sm border-amber-200 bg-amber-200 rounded-md shadow shadow-amber-100'
+          />
         </div>
       </div>
     </main>
