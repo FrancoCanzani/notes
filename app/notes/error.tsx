af9435251f21bd64370c1f03f584078e83b2994b
Franco Canzani <francocanzani@gmail.com>
2024-03-21 22:20:22 +0100

    improves error page

--- app/notes/error.tsx
+++ app/notes/error.tsx
@@ -1,5 +1,7 @@
 'use client'; // Error components must be Client Components
 
+import { Button } from '../components/ui/button';
+
 export default function Error({
   error,
   reset,
@@ -8,17 +10,20 @@ export default function Error({
   reset: () => void;
 }) {
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-      <p>{`Error: ${error}`}</p>
+    <div className='flex items-center min-h-screen p-4 text-center md:justify-center md:p-6'>
+      <div className='space-y-6'>
+        <div className='space-y-2'>
+          <h1 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl'>
+            Oops, something went wrong!
+          </h1>
+          <p className='mx-auto max-w-[400px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400'>
+            The page failed to load.
+          </p>
+        </div>
+        <div>
+          <Button onClick={() => reset()}>Try Again</Button>
+        </div>
+      </div>
     </div>
   );
 }
