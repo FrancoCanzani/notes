c5ff03f66fc8ba9f32516d5afa9dcd796b5d54fe
Franco Canzani <francocanzani@gmail.com>
2024-05-12 21:08:34 +0200

    fix auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,14 +10,12 @@ export const {
   secret: process.env.NEXTAUTH_SECRET as string,
   callbacks: {
     async jwt({ token, profile }) {
-      // Persist the OAuth access_token and or the user id to the token right after signin
       if (profile && profile.id) {
         token.id = profile.id;
       }
       return token;
     },
     async session({ session, token, user }) {
-      // Send properties to the client, like an access_token and user id from a provider.
       session.user.id = token.id;
       return session;
     },
