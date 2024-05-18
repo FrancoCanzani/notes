2c0ef496d93afbc11c0a9abdff0145761f08ad7b
Franco Canzani <francocanzani@gmail.com>
2024-05-18 11:17:55 +0200

    fix notes redirect

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,42 +1,31 @@
+import { auth } from '@clerk/nextjs/server';
 import getCloudNotes from '../../lib/helpers/get-cloud-notes';
 import { Note } from '../../lib/types';
 import { nanoid } from 'nanoid';
 import { redirect } from 'next/navigation';
-import { auth } from '@clerk/nextjs/server';
 
 export default async function Page() {
+  const { userId } = auth();
   const newNoteId = nanoid(7);
 
-  try {
-    const { userId } = auth();
-
-    if (!userId) {
-      console.error('User not authenticated');
-      redirect(`/`);
-    }
-
+  if (userId) {
     const notes = await getCloudNotes(userId);
-
-    if (!notes) {
-      console.error('No notes found for user:', userId);
-      redirect(`/notes/${newNoteId}`);
-    }
-
     const parsedNotes: Note[] = JSON.parse(JSON.stringify(notes));
 
-    if (parsedNotes.length === 0) {
-      console.log('No notes available for user:', userId);
-      redirect(`/notes/${newNoteId}`);
-    }
-
     const sortedNotes = parsedNotes.sort((a, b) => {
-      return new Date(b.lastSaved).getTime() - new Date(a.lastSaved).getTime();
+      if (new Date(a.lastSaved) > new Date(b.lastSaved)) {
+        return -1;
+      }
+      if (new Date(a.lastSaved) < new Date(b.lastSaved)) {
+        return 1;
+      }
+      return 0;
     });
 
-    console.log('Redirecting to the last edited note:', sortedNotes[0].id);
-    redirect(`/notes/${sortedNotes[0].id}`);
-  } catch (error) {
-    console.error('An error occurred:', error);
-    redirect(`/notes/${newNoteId}`);
+    if (sortedNotes) {
+      redirect(`/notes/${sortedNotes[0].id}`);
+    } else {
+      redirect(`/notes/${newNoteId}`);
+    }
   }
 }
