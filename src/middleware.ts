d6092488fcee21708489796b1607d9f92d16a265
Franco Canzani <francocanzani@gmail.com>
2024-05-19 11:38:13 +0200

    adds redirect in landing page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 const isProtectedRoute = createRouteMatcher(['/notes(.*)', '/calendar(.*)']);
 
 export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
+  const url = req.nextUrl.clone();
+
   if (isProtectedRoute(req)) auth().protect();
+
+  // Check if the current path is '/' and the user is authenticated
+  if (url.pathname === '/' && userId) {
+    // Redirect to '/notes'
+    url.pathname = '/notes';
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
 });
 
 export const config = {
