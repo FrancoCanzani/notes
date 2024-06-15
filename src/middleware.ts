b69862606200e7b916d679182e0da14122e9a674
Franco Canzani <francocanzani@gmail.com>
2024-06-15 21:03:37 +0200

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,15 +5,13 @@ const isProtectedRoute = createRouteMatcher(['/notes(.*)', '/todos(.*)']);
 
 export default clerkMiddleware((auth, req) => {
   const { userId } = auth();
-  const url = req.nextUrl.clone();
+  const url = req.nextUrl;
 
   if (isProtectedRoute(req)) auth().protect();
 
   // Check if the current path is '/' and the user is authenticated
   if (url.pathname === '/' && userId) {
-    // Redirect to '/notes'
-    url.pathname = '/notes';
-    return NextResponse.redirect(url);
+    return NextResponse.redirect(new URL(`/notes`, req.url));
   }
 
   return NextResponse.next();
