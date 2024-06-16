62b7ef9b8a31d7fcc1f698997948fa1ab4461193
Franco Canzani <francocanzani@gmail.com>
2024-06-16 17:25:08 +0200

    fix

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -17,26 +17,22 @@ export default async function Page() {
 
     return (
       <div className='min-h-screen flex flex-col lg:flex-row w-full overflow-x-hidden'>
-        <div className='bg-neutral-50 flex-1 flex flex-col h-full'>
+        <div className='bg-neutral-50 w-full lg:flex-1 lg:h-[calc(100vh)]'>
           <div className='flex px-5 pt-4 items-center justify-start gap-x-3'>
             <NavDrawer notes={parsedNotes} />
             <h2 className='font-medium text-xl capitalize'>Calendar</h2>
           </div>
-          <div className='flex-1 overflow-auto'>
-            <TodosNavbar todos={parsedTodos} />
-          </div>
+          <TodosNavbar todos={parsedTodos} />
         </div>
-        <div className='flex-1 flex flex-col h-full'>
+        <div>
           <h2 className='flex items-center justify-between px-5 pt-4 font-medium text-xl capitalize'>
             Todos
           </h2>
-          <div className='p-5 flex-1 pt-3 gap-3 w-full overflow-auto'>
+          <div className='p-5 flex-1 pt-3 gap-3 w-full lg:flex-1'>
             <Todos todos={parsedTodos} />
           </div>
         </div>
       </div>
     );
   }
-
-  return null;
 }
