90884b0d666e9f1454b5f9f4ec0826dd90ce0063
Franco Canzani <francocanzani@gmail.com>
2024-08-19 13:39:51 +0000

    fix drawer colors

--- src/components/nav-drawer.tsx
+++ src/components/nav-drawer.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { cn } from '../lib/utils';
-import { useAuth } from '@clerk/nextjs';
-import { Note } from '../lib/types';
-import { nanoid } from 'nanoid';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "../lib/utils";
+import { useAuth } from "@clerk/nextjs";
+import { Note } from "../lib/types";
+import { nanoid } from "nanoid";
 import {
   Drawer,
   DrawerClose,
@@ -15,35 +15,35 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
-} from './ui/drawer';
-import { FileIcon, DrawingPinFilledIcon } from '@radix-ui/react-icons';
-import { Input } from './ui/input';
-import { Button } from './ui/button';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { toast } from 'sonner';
-import { saveNote } from '../lib/actions';
+} from "./ui/drawer";
+import { FileIcon, DrawingPinFilledIcon } from "@radix-ui/react-icons";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { saveNote } from "../lib/actions";
 
 export default function NavDrawer({ notes }: { notes?: Note[] }) {
   const pathname = usePathname();
   const { userId } = useAuth();
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
   const noteId = nanoid(7);
   const router = useRouter();
 
   async function handleSubmit() {
     if (userId && title.trim()) {
-      toast.promise(saveNote(userId, noteId, title, ''), {
-        loading: 'Saving note...',
+      toast.promise(saveNote(userId, noteId, title, ""), {
+        loading: "Saving note...",
         success: async (data) => {
           await router.push(`/notes/${noteId}`);
-          setTitle('');
-          return 'Note created successfully';
+          setTitle("");
+          return "Note created successfully";
         },
-        error: 'Error saving note',
+        error: "Error saving note",
       });
     } else if (!title.trim()) {
-      toast.error('Please enter note title');
+      toast.error("Please enter note title");
     }
   }
 
@@ -53,50 +53,50 @@ export default function NavDrawer({ notes }: { notes?: Note[] }) {
 
   const activeNotes = notes
     ? notes.filter(
-        (note: Note) => note.status === 'active' && note.pinned != true
+        (note: Note) => note.status === "active" && note.pinned != true
       )
     : [];
 
   return (
     <Drawer>
-      <DrawerTrigger asChild className='sm:hidden'>
+      <DrawerTrigger asChild className="sm:hidden">
         <button>
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='1.5rem'
-            height='1.5rem'
-            viewBox='0 0 16 16'
+            xmlns="http://www.w3.org/2000/svg"
+            width="1.5rem"
+            height="1.5rem"
+            viewBox="0 0 16 16"
           >
             <path
-              fill='currentColor'
-              d='M10.01 8.5c0-.276.216-.5.495-.5h2.01c.243 0 .445.183.487.412l.008.088c0 .276-.216.5-.495.5h-2.01a.5.5 0 0 1-.487-.412zM12.5 12c.25 0 .459.183.502.412l.008.088c0 .276-.228.5-.51.5H3.52a.51.51 0 0 1-.502-.412L3.01 12.5c0-.276.228-.5.51-.5h3.987V4.208l-2.06 2.06a.5.5 0 1 1-.707-.707L6.86 3.44A1.5 1.5 0 0 1 7.974 3h.033q.06 0 .118.014c.314.043.616.185.857.426l2.122 2.12a.5.5 0 0 1-.708.708l-1.889-1.89V12zM3 8.5c0-.276.216-.5.495-.5h2.01c.243 0 .445.183.487.412L6 8.5c0 .276-.216.5-.495.5h-2.01a.5.5 0 0 1-.487-.412z'
+              fill="currentColor"
+              d="M10.01 8.5c0-.276.216-.5.495-.5h2.01c.243 0 .445.183.487.412l.008.088c0 .276-.216.5-.495.5h-2.01a.5.5 0 0 1-.487-.412zM12.5 12c.25 0 .459.183.502.412l.008.088c0 .276-.228.5-.51.5H3.52a.51.51 0 0 1-.502-.412L3.01 12.5c0-.276.228-.5.51-.5h3.987V4.208l-2.06 2.06a.5.5 0 1 1-.707-.707L6.86 3.44A1.5 1.5 0 0 1 7.974 3h.033q.06 0 .118.014c.314.043.616.185.857.426l2.122 2.12a.5.5 0 0 1-.708.708l-1.889-1.89V12zM3 8.5c0-.276.216-.5.495-.5h2.01c.243 0 .445.183.487.412L6 8.5c0 .276-.216.5-.495.5h-2.01a.5.5 0 0 1-.487-.412z"
             />
-          </svg>{' '}
-          <span className='sr-only'>Toggle Menu</span>
+          </svg>{" "}
+          <span className="sr-only">Toggle Menu</span>
         </button>
       </DrawerTrigger>
-      <DrawerContent className='bg-quarter-spanish-white-100'>
-        <div className='mx-auto w-full max-w-sm'>
+      <DrawerContent className="bg-bermuda-gray-100">
+        <div className="mx-auto w-full max-w-sm">
           <DrawerHeader>
             <DrawerTitle>
-              <h1 className='font-semibold py-1.5'>Notes</h1>
+              <h1 className="font-semibold py-1.5">Notes</h1>
             </DrawerTitle>
           </DrawerHeader>
-          <div className='flex flex-col justify-between w-full px-1.5 py-4 min-h-60'>
-            <div className='space-y-4'>
+          <div className="flex flex-col justify-between w-full px-1.5 py-4 min-h-60">
+            <div className="space-y-4">
               <form
                 action={handleSubmit}
-                className='flex items-center justify-between gap-x-2'
+                className="flex items-center justify-between gap-x-2"
               >
                 <Input
-                  className='text-center focus:text-start bg-quarter-spanish-white-50 border-none placeholder:font-medium focus-visible:ring-0 focus-visible:ring-offset-0 outline-none font-medium text-sm w-full'
-                  placeholder='New Note Title'
+                  className="text-center focus:text-start bg-bermuda-gray-50 border-none placeholder:font-medium focus-visible:ring-0 focus-visible:ring-offset-0 outline-none font-medium text-sm w-full"
+                  placeholder="New Note Title"
                   onChange={(e) => setTitle(e.target.value)}
                 />
                 <Button
-                  variant={'menu'}
-                  size={'lg'}
-                  className='px-3 py-2 h-10 rounded-md font-medium bg-quarter-spanish-white-50'
+                  variant={"menu"}
+                  size={"lg"}
+                  className="px-3 py-2 h-10 rounded-md font-medium bg-bermuda-gray-50"
                 >
                   Create
                 </Button>
@@ -106,19 +106,19 @@ export default function NavDrawer({ notes }: { notes?: Note[] }) {
                   <Link href={`/notes/${note.id}`} key={note._id}>
                     <div
                       className={cn(
-                        'p-1.5 my-1 bg-quarter-spanish-white-100 rounded-lg text-sm w-full flex-col items-center justify-between hover:bg-quarter-spanish-white-200',
+                        "p-1.5 my-1 bg-bermuda-gray-100 rounded-lg text-sm w-full flex-col items-center justify-between hover:bg-bermuda-gray-200",
                         pathname.includes(note.id) &&
-                          'bg-quarter-spanish-white-200 font-semibold'
+                          "bg-bermuda-gray-200 font-semibold"
                       )}
                     >
-                      <div className='flex items-center justify-between w-full'>
-                        <div className='flex items-center w-auto justify-start gap-x-2'>
+                      <div className="flex items-center justify-between w-full">
+                        <div className="flex items-center w-auto justify-start gap-x-2">
                           {note.pinned ? (
                             <DrawingPinFilledIcon />
                           ) : (
                             <FileIcon />
                           )}
-                          <p title={note.title} className='truncate max-w-64'>
+                          <p title={note.title} className="truncate max-w-64">
                             {note.title}
                           </p>
                         </div>
--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import * as React from 'react';
-import { Drawer as DrawerPrimitive } from 'vaul';
-import { cn } from '../../lib/utils';
+import * as React from "react";
+import { Drawer as DrawerPrimitive } from "vaul";
+import { cn } from "../../lib/utils";
 
 const Drawer = ({
   shouldScaleBackground = true,
@@ -13,7 +13,7 @@ const Drawer = ({
     {...props}
   />
 );
-Drawer.displayName = 'Drawer';
+Drawer.displayName = "Drawer";
 
 const DrawerTrigger = DrawerPrimitive.Trigger;
 
@@ -27,7 +27,7 @@ const DrawerOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Overlay
     ref={ref}
-    className={cn('fixed inset-0 z-50 bg-black/80', className)}
+    className={cn("fixed inset-0 z-50 bg-black/80", className)}
     {...props}
   />
 ));
@@ -42,39 +42,39 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background',
+        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
         className
       )}
       {...props}
     >
-      <div className='mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted' />
+      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted" />
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>
 ));
-DrawerContent.displayName = 'DrawerContent';
+DrawerContent.displayName = "DrawerContent";
 
 const DrawerHeader = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
-    className={cn('grid gap-1.5 p-4 text-center sm:text-left', className)}
+    className={cn("grid gap-1.5 p-2 text-center sm:text-left", className)}
     {...props}
   />
 );
-DrawerHeader.displayName = 'DrawerHeader';
+DrawerHeader.displayName = "DrawerHeader";
 
 const DrawerFooter = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
-    className={cn('mt-auto flex flex-col gap-2 p-4', className)}
+    className={cn("mt-auto flex flex-col gap-2 p-2", className)}
     {...props}
   />
 );
-DrawerFooter.displayName = 'DrawerFooter';
+DrawerFooter.displayName = "DrawerFooter";
 
 const DrawerTitle = React.forwardRef<
   React.ElementRef<typeof DrawerPrimitive.Title>,
@@ -83,7 +83,7 @@ const DrawerTitle = React.forwardRef<
   <DrawerPrimitive.Title
     ref={ref}
     className={cn(
-      'text-lg font-semibold leading-none tracking-tight',
+      "text-lg font-semibold leading-none tracking-tight",
       className
     )}
     {...props}
@@ -97,7 +97,7 @@ const DrawerDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Description
     ref={ref}
-    className={cn('text-sm text-muted-foreground', className)}
+    className={cn("text-sm text-muted-foreground", className)}
     {...props}
   />
 ));
