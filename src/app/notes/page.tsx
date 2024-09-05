b9c5f6f408b93754e3f6c61889d5282e3702f11a
Franco Canzani <francocanzani@gmail.com>
2024-09-06 00:42:48 +0200

    fix

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,8 +1,7 @@
 import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
 import Sidebar from '@/components/sidebar/sidebar';
 import fetchNotes from '@/lib/helpers/fetch-notes';
-import isMobile from '@/lib/helpers/is-mobile';
+import NavDrawer from '@/components/nav-drawer';
 
 export default async function Page() {
   const { userId } = await auth();
@@ -10,16 +9,6 @@ export default async function Page() {
   if (userId) {
     const notes = await fetchNotes(userId);
     const serializedNotes = JSON.parse(JSON.stringify(notes));
-    const usesMobile = isMobile();
-
-    // Redirect to the last note if it's a mobile device and there are notes
-    if (usesMobile && serializedNotes.length > 0) {
-      const lastNote = serializedNotes[serializedNotes.length - 1];
-      console.log(lastNote);
-      console.log('Hello');
-
-      redirect(`/notes/${lastNote.id}`);
-    }
 
     return (
       <div className='w-full sm:flex items-start'>
@@ -27,13 +16,16 @@ export default async function Page() {
           <Sidebar notes={serializedNotes} />
         </aside>
         <main className='flex-grow flex items-center justify-center h-screen bg-bermuda-gray-50'>
-          <div className='text-center space-y-4'>
+          <div className='text-center space-y-4 px-4'>
             <h2 className='text-3xl font-bold text-bermuda-gray-950'>
               Welcome to Your Notes!
             </h2>
             <p className='text-xl text-bermuda-gray-700'>
               Select a note to edit or create a new one to get started.
             </p>
+            <NavDrawer notes={serializedNotes}>
+              <button className='sm:hidden underline'>Open Menu</button>
+            </NavDrawer>
           </div>
         </main>
       </div>
